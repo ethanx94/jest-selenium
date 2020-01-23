2aeffb613612f5e3ee3d336317ce5f94177f5c0e
Ethan Richardson <ethanx94@gmail.com>
2020-01-22 21:01:08 -0500

    update, use chrome

--- index.test.js
+++ index.test.js
@@ -1,27 +1,27 @@
-const { Builder, By, Key, until } = require('selenium-webdriver')
-require('selenium-webdriver/chrome')
-require('selenium-webdriver/firefox')
-require('chromedriver')
-require('geckodriver')
-const { querySelector } = require('./helpers')
+const {
+  Builder, By, Key, until,
+} = require('selenium-webdriver');
+require('selenium-webdriver/chrome');
+require('chromedriver');
+const { querySelector } = require('./helpers');
 
-const rootURL = 'https://www.mozilla.org/en-US/'
-let driver
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
+const rootURL = 'https://www.mozilla.org/en-US/';
+let driver;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5;
 
 beforeAll(async () => {
-  driver = await new Builder().forBrowser('firefox').build()
-})
+  driver = await new Builder().forBrowser('chrome').build();
+});
 
-afterAll(async () => driver.quit())
+afterAll(async () => driver.quit());
 
 it('initialises the context', async () => {
-  await driver.get(rootURL)
-})
+  await driver.get(rootURL);
+});
 
 it('should click on navbar button to display a drawer', async () => {
-  const anchor = await querySelector('[href=\'/en-US/firefox/\']', driver)
-  const actual = await anchor.getText()
-  const expected = 'Firefox'
-  expect(actual).toEqual(expected)
-})
+  const anchor = await querySelector('[href=\'/en-US/firefox/\']', driver);
+  const actual = await anchor.getText();
+  const expected = 'Firefox';
+  expect(actual).toEqual(expected);
+});
